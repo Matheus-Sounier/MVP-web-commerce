4ecd0bb10915845d7e373d1267c3af9f7d239862
Matheus-Sounier <matheus.sounier2024@gmail.com>
2026-06-14 22:56:18 -0400

    fix: corrects debug configuration

--- flux_store/settings.py
+++ flux_store/settings.py
@@ -25,7 +25,6 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ['*']
